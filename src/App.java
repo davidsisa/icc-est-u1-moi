5ea2b2eb7d1712b42269e39e9f48da7e1ebdb0ed
S0mbr4xc <s3._xc@hotmail.com>
2024-09-27 12:18:46 -0500

    Ordenar arreglo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("***** Metodo de Ordenamiento *****");
+        OrdenarSort ordenar = new OrdenarSort ();
+        int[] array1 = {10,5,7,0};
+        int[] arrayOrd = ordenar.sortBySeleccion(array1);
+        System.out.println(" - Arreglo Ordenado: ");
+        ordenar.printArreglo(arrayOrd);
     }
 }
---
+++ src/OrdenarSort.java
@@ -0,0 +1,25 @@
+public class OrdenarSort {
+    public int[] sortBySeleccion(int[] array){
+        int tamanio = array.length;
+        //recorrer arreglo
+        for (int i = 0; i < tamanio; i++) {
+            int indice = i;
+            for (int j = i+1; j < tamanio; j++) {
+                if (array[j] > array[indice]){
+                    indice = j;
+                }
+            }
+            //intercambiar del indice encontrado
+            //por el primero
+            int aux = array[indice];
+            array[indice] = array [i];
+            array[i] = aux;
+        }
+        return array;
+    }
+    public void printArreglo (int[] array){
+        for (Object elem : array) {
+            System.out.print(" "+ elem);
+        }
+    }
+}
